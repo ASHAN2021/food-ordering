c4a5f5cea11c3465e81bfce2bb67f2011d98f253
ASHAN2021 <akh8130@gmail.com>
2024-12-16 21:27:14 +0530

    implement Food controller and food service

--- src/main/java/com/zosh/request/CreateFoodRequest.java
+++ src/main/java/com/zosh/request/CreateFoodRequest.java
@@ -2,9 +2,11 @@ package com.zosh.request;
 
 import com.zosh.model.Category;
 import com.zosh.model.IngrediantsItem;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class CreateFoodRequest {
 
     private String name;
